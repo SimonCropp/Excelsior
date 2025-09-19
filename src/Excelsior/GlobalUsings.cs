dbe66322f037b227065e360d2021c9cc02a7d77d
Simon Cropp <simon.cropp@gmail.com>
2025-09-19 22:53:49 +1000

    cleanup

--- src/Excelsior/GlobalUsings.cs
+++ src/Excelsior/GlobalUsings.cs
@@ -1,4 +1,5 @@
 ﻿global using System.ComponentModel;
 global using System.ComponentModel.DataAnnotations;
+global using System.Diagnostics.CodeAnalysis;
 global using ClosedXML.Excel;
 global using System.Linq.Expressions;

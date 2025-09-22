4d6724e58eaaa7748d4c5b3f955cb34c14ef13f2
Simon Cropp <simon.cropp@gmail.com>
2025-09-22 20:58:39 +1000

    .

--- src/Excelsior/GlobalUsings.cs
+++ src/Excelsior/GlobalUsings.cs
@@ -5,3 +5,5 @@ global using ClosedXML.Excel;
 global using System.Linq.Expressions;
 global using Sheet = ClosedXML.Excel.IXLWorksheet;
 global using Book = ClosedXML.Excel.IXLWorkbook;
+global using Cell = ClosedXML.Excel.IXLCell;
+

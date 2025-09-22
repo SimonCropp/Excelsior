658bc7865f8c7bef56c2b04fe681e1f0a81105c5
Simon Cropp <simon.cropp@gmail.com>
2025-09-22 17:48:03 +1000

    .

---
+++ src/Tests/EnumerableStringTests.cs
@@ -0,0 +1,31 @@
+﻿[TestFixture]
+public class EnumerableStringTests
+{
+    public record Person(string Name, string[] PhoneNumbers);
+
+    [Test]
+    public async Task Test()
+    {
+        #region ComplexTypeWithToString
+
+        List<Person> data =
+        [
+            new("John Doe",
+                PhoneNumbers:
+                [
+                    "+1 3057380950",
+                    "+1 5056169368",
+                    "+1 8634446859"
+                ]),
+        ];
+
+        var builder = new BookBuilder();
+        builder.AddSheet(data);
+
+        #endregion
+
+        var book = await builder.Build();
+
+        await Verify(book);
+    }
+}

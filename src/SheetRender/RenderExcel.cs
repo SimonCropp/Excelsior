09abfe667ddbe34ee40ec8193d5f57ef31803d72
Simon Cropp <simon.cropp@gmail.com>
2025-09-26 15:25:22 +1000

    Update RenderExcel.cs

--- src/SheetRender/RenderExcel.cs
+++ src/SheetRender/RenderExcel.cs
@@ -17,8 +17,8 @@ public class RenderExcel
 
         foreach (var file in imageFiles)
         {
-            if (file.Contains(".verified.") &&
-                !file.EndsWith("icon.png"))
+            if (file.Contains(".verified.") ||
+                file.EndsWith("icon.png"))
             {
                 continue;
             }

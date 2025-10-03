b0f1e46708c07e4017697df9f6381fec936ece9c
Simon Cropp <simon.cropp@gmail.com>
2025-10-03 20:16:46 +1000

    .

---
+++ src/Excelsior/SheetBuilderBase.cs
@@ -0,0 +1,118 @@
+﻿namespace Excelsior;
+
+public abstract partial class SheetBuilderBase<TModel, TStyle, TCell>:
+    ISheetBuilder<TModel, TStyle, TCell>
+{
+    public abstract ISheetBuilder<TModel, TStyle, TCell> Column<TProperty>(Expression<Func<TModel, TProperty>> property, Action<Column<TStyle, TModel, TProperty>> configuration);
+    protected abstract void SetDateFormat(TStyle style, string format);
+    protected abstract void SetNumberFormat(TStyle style, string format);
+    protected abstract void SetCellValue(TCell cell, object value);
+    protected abstract void SetCellHtml(TCell cell, string value);
+    protected abstract void WriteEnumerable(TCell cell, IEnumerable<string> enumerable);
+    internal void SetCellValue(TCell cell, TStyle style, object? value, Column<TStyle, TModel> column, TModel item, bool trimWhitespace)
+    {
+        void SetStringOrHtml(string content)
+        {
+            if (column.IsHtml)
+            {
+                SetCellHtml(cell, content);
+            }
+            else
+            {
+                SetCellValue(cell, content);
+            }
+        }
+
+        void ThrowIfHtml()
+        {
+            if (column.IsHtml)
+            {
+                throw new("TreatAsHtml is not compatible with this type");
+            }
+        }
+
+        if (value == null)
+        {
+            if (column.NullDisplay != null)
+            {
+                SetCellValue(cell, column.NullDisplay);
+            }
+
+            return;
+        }
+
+        if (column.Render != null)
+        {
+            var render = column.Render(item, value);
+            if (render != null)
+            {
+                SetStringOrHtml(render);
+            }
+
+            return;
+        }
+
+        if (value is DateTime dateTime)
+        {
+            ThrowIfHtml();
+            SetCellValue(cell, dateTime);
+            SetDateFormat(style, column.Format ?? ValueRenderer.DefaultDateTimeFormat);
+
+            return;
+        }
+
+        if (value is Date date)
+        {
+            ThrowIfHtml();
+            SetCellValue(cell, date.ToDateTime(new(0, 0)));
+            SetDateFormat(style, column.Format ?? ValueRenderer.DefaultDateFormat);
+
+            return;
+        }
+
+        if (value is bool boolean)
+        {
+            ThrowIfHtml();
+            SetCellValue(cell, boolean);
+            return;
+        }
+
+        if (value is Enum enumValue)
+        {
+            ThrowIfHtml();
+            SetCellValue(cell, enumValue.DisplayName());
+            return;
+        }
+
+        if (column.IsNumber)
+        {
+            ThrowIfHtml();
+            SetCellValue(cell, Convert.ToDouble(value));
+            if (column.Format != null)
+            {
+                SetNumberFormat(style, column.Format);
+            }
+
+            return;
+        }
+
+        if (value is IEnumerable<string> enumerable)
+        {
+            ThrowIfHtml();
+            WriteEnumerable(cell, enumerable);
+            return;
+        }
+
+        var valueAsString = value.ToString();
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+        if (valueAsString != null && trimWhitespace)
+        {
+            valueAsString = valueAsString.Trim();
+        }
+
+        if (valueAsString != null)
+        {
+            SetStringOrHtml(valueAsString);
+        }
+    }
+}
--- src/Excelsior/SheetBuilderExtensions.cs
+++ src/Excelsior/SheetBuilderExtensions.cs
@@ -1,71 +1,58 @@
-// ConvertToExtensionBlock doesnt work properly in net9 yet
-// ReSharper disable ConvertToExtensionBlock
 namespace Excelsior;
 
-public static class SheetBuilderExtensions
+public abstract partial class SheetBuilderBase<TModel, TStyle, TCell>
 {
-    public static void HeadingText<TModel, TStyle, TCell, TProperty>(
-        this ISheetBuilder<TModel, TStyle, TCell> builder,
+    public void HeadingText<TProperty>(
         Expression<Func<TModel, TProperty>> property,
         string value) =>
-        builder.Column(property, _ => _.Heading = value);
+        Column(property, _ => _.Heading = value);
 
-    public static void Order<TModel, TStyle, TCell, TProperty>(
-        this ISheetBuilder<TModel, TStyle, TCell> builder,
+    public void Order<TProperty>(
         Expression<Func<TModel, TProperty>> property,
         int? value) =>
-        builder.Column(property, _ => _.Order = value);
+        Column(property, _ => _.Order = value);
 
-    public static void Width<TModel, TStyle, TCell, TProperty>(
-        this ISheetBuilder<TModel, TStyle, TCell> builder,
+    public void Width<TProperty>(
         Expression<Func<TModel, TProperty>> property,
         double? value) =>
-        builder.Column(property, _ => _.Width = value);
+        Column(property, _ => _.Width = value);
 
-    public static void HeadingStyle<TModel, TStyle, TCell, TProperty>(
-        this ISheetBuilder<TModel, TStyle, TCell> builder,
+    public void HeadingStyle<TProperty>(
         Expression<Func<TModel, TProperty>> property,
         Action<TStyle> value) =>
-        builder.Column(property, _ => _.HeadingStyle = value);
+        Column(property, _ => _.HeadingStyle = value);
 
-    public static void CellStyle<TModel, TStyle, TCell, TProperty>(
-        this ISheetBuilder<TModel, TStyle, TCell> builder,
+    public void CellStyle<TProperty>(
         Expression<Func<TModel, TProperty>> property,
         Action<TStyle, TModel, TProperty> value) =>
-        builder.Column(property, _ => _.CellStyle = value);
+        Column(property, _ => _.CellStyle = value);
 
-    public static void CellStyle<TModel, TStyle, TCell, TProperty>(
-        this ISheetBuilder<TModel, TStyle, TCell> builder,
+    public void CellStyle<TProperty>(
         Expression<Func<TModel, TProperty>> property,
         Action<TStyle, TProperty> value) =>
-        builder.Column(property, _ => _.CellStyle  = (style, _, property) => value(style, property));
+        Column(property, _ => _.CellStyle = (style, _, property) => value(style, property));
 
-    public static void Format<TModel, TStyle, TCell, TProperty>(
-        this ISheetBuilder<TModel, TStyle, TCell> builder,
+    public void Format<TProperty>(
         Expression<Func<TModel, TProperty>> property,
         string value) =>
-        builder.Column(property, _ => _.Format = value);
+        Column(property, _ => _.Format = value);
 
-    public static void NullDisplay<TModel, TStyle, TCell, TProperty>(
-        this ISheetBuilder<TModel, TStyle, TCell> builder,
+    public void NullDisplay<TProperty>(
         Expression<Func<TModel, TProperty>> property,
         string value) =>
-        builder.Column(property, _ => _.NullDisplay = value);
+        Column(property, _ => _.NullDisplay = value);
 
-    public static void IsHtml<TModel, TStyle, TCell, TProperty>(
-        this ISheetBuilder<TModel, TStyle, TCell> builder,
+    public void IsHtml<TProperty>(
         Expression<Func<TModel, TProperty>> property) =>
-        builder.Column(property, _ => _.IsHtml = true);
+        Column(property, _ => _.IsHtml = true);
 
-    public static void Render<TModel, TStyle, TCell, TProperty>(
-        this ISheetBuilder<TModel, TStyle, TCell> builder,
+    public void Render<TProperty>(
         Expression<Func<TModel, TProperty>> property,
         Func<TModel, TProperty, string?> value) =>
-        builder.Column(property, _ => _.Render = value);
+        Column(property, _ => _.Render = value);
 
-    public static void Render<TModel, TStyle, TCell, TProperty>(
-        this ISheetBuilder<TModel, TStyle, TCell> builder,
+    public void Render<TProperty>(
         Expression<Func<TModel, TProperty>> property,
         Func<TProperty, string?> value) =>
-        builder.Column(property, _ => _.Render = (_, property) => value(property));
+        Column(property, _ => _.Render = (_, property) => value(property));
 }

d475e324686e9497985fdb0f7bb250b3ec82f0af
Simon Cropp <simon.cropp@gmail.com>
2025-10-05 11:02:12 +1100

    .

--- src/ExcelsiorClosedXml/Renderer.cs
+++ src/ExcelsiorClosedXml/Renderer.cs
@@ -68,47 +68,8 @@
     protected override void SetCellHtml(Cell cell, string value) =>
         throw new("ClosedXml does not support html");
 
-    protected override void WriteEnumerable(Cell cell, IEnumerable<string> enumerable)
-    {
-        var rich = cell.CreateRichText();
-        var list = enumerable.ToList();
-        var builder = new StringBuilder();
-        for (var index = 0; index < list.Count; index++)
-        {
-            var item = list[index];
-            rich.AddText("• ").SetBold();
-            foreach (var line in item.AsSpan().EnumerateLines())
-            {
-                if (trimWhitespace)
-                {
-                    if (line.Length == 0)
-                    {
-                        continue;
-                    }
-
-                    builder.Append(line.Trim());
-                }
-                else
-                {
-                    builder.Append(line);
-                }
-
-                builder.Append("\n   ");
-            }
-
-            if (index < list.Count - 1)
-            {
-                builder.Length -= 3;
-            }
-            else
-            {
-                builder.Length -= 4;
-            }
-
-            rich.AddText(builder.ToString());
-            builder.Clear();
-        }
-    }
+    protected override void WriteEnumerable(Cell cell, IEnumerable<string> enumerable) =>
+        RichText.Enumerable(cell, enumerable, trimWhitespace);
 
     void ApplyHeadingStyling(Cell cell, Column<Style, TModel> column)
     {

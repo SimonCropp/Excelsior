1d83b0f8033474829d392b447f2bd5c1153b6011
Simon Cropp <simon.cropp@gmail.com>
2025-10-07 08:41:05 +1100

    .

--- src/ExcelsiorSyncfusion.Tests/GlobalUsings.cs
+++ src/ExcelsiorSyncfusion.Tests/GlobalUsings.cs
@@ -1,7 +1,6 @@
 ﻿global using System.ComponentModel;
 global using System.ComponentModel.DataAnnotations;
 global using System.Globalization;
-global using ClosedXML.Excel;
 global using Excelsior;
 global using ExcelsiorClosedXml;
 global using Syncfusion.Licensing;

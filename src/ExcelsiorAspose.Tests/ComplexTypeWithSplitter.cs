92f308b00610e1f784b467ffb87ee4041c206086
Simon Cropp <simon.cropp@gmail.com>
2025-10-10 19:07:40 +1100

    .

--- src/ExcelsiorAspose.Tests/ComplexTypeWithSplitter.cs
+++ src/ExcelsiorAspose.Tests/ComplexTypeWithSplitter.cs
@@ -2,77 +2,36 @@
 [TestFixture]
 public class ComplexTypeWithSplitter
 {
-    public record Target(
-        string Property,
-        [Split]
-        Target.NestedTarget Nested)
+    public enum State
     {
-        public record NestedTarget(string NestedProperty);
+        SA
     }
+
+    public record Person(string Name, Address Address);
+
+    public record Address(int Number, string Street, string City, State State, ushort PostCode);
 
     [Test]
-    public async Task NestedColumn()
+    public async Task Test()
     {
+        #region ComplexTypeWithSplitter
+
         var builder = new BookBuilder();
 
-        List<Target> data =
+        List<Person> data =
         [
-            new("the Property",
-                new Target.NestedTarget(
-                    NestedProperty: "the NestedProperty")),
-        ];
-        var sheet = builder.AddSheet(data);
-        sheet.Column(_ => _.Nested.NestedProperty, _ => _.Heading = "Custom");
-        var book = await builder.Build();
-
-        await Verify(book);
-    }
-
-    public record TargetMultipleOverlappingNested(
-        string Property,
-        [Split]
-        TargetMultipleOverlappingNested.NestedTarget Nested1,
-        [Split]
-        TargetMultipleOverlappingNested.NestedTarget Nested2)
-    {
-        public record NestedTarget(string NestedProperty);
-    }
-
-    [Test]
-    public async Task MultipleOverlappingNested()
-    {
-        var builder = new BookBuilder();
-
-        List<TargetMultipleOverlappingNested> data =
-        [
-            new("the Property",
-                new TargetMultipleOverlappingNested.NestedTarget(
-                    NestedProperty: "the NestedProperty 1"),
-                new TargetMultipleOverlappingNested.NestedTarget(
-                    NestedProperty: "the NestedProperty 2")),
+            new("John Doe",
+                new Address(
+                    Number: 900,
+                    Street: "Victoria Square",
+                    City: "Adelaide",
+                    State: State.SA,
+                    PostCode: 5000)),
         ];
         builder.AddSheet(data);
-        var book = await builder.Build();
 
-        await Verify(book);
-    }
+        #endregion
 
-    [Test]
-    public async Task MultipleOverlappingNestedAndColumnConfig()
-    {
-        var builder = new BookBuilder();
-
-        List<TargetMultipleOverlappingNested> data =
-        [
-            new("the Property",
-                new TargetMultipleOverlappingNested.NestedTarget(
-                    NestedProperty: "the NestedProperty 1"),
-                new TargetMultipleOverlappingNested.NestedTarget(
-                    NestedProperty: "the NestedProperty 2")),
-        ];
-        var sheet = builder.AddSheet(data);
-        sheet.Column(_ => _.Nested1.NestedProperty, _ => _.Heading = "Custom1");
-        sheet.Column(_ => _.Nested2.NestedProperty, _ => _.Heading = "Custom2");
         var book = await builder.Build();
 
         await Verify(book);

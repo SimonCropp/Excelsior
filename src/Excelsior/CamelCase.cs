18b984094f01136f9b73712c8c5a92c7c91af0d5
Simon Cropp <simon.cropp@gmail.com>
2025-10-12 22:56:27 +1100

    Update CamelCase.cs

--- src/Excelsior/CamelCase.cs
+++ src/Excelsior/CamelCase.cs
@@ -2,7 +2,7 @@
 {
     public static string Split(string text)
     {
-        var result = new StringBuilder();
+        var result = new StringBuilder(text.Length);
         for (var i = 0; i < text.Length; i++)
         {
             if (i > 0 && char.IsUpper(text[i]))

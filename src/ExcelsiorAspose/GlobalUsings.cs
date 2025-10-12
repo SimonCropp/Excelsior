19ab432dee319cbc2778a7f17a8d983433528eec
Simon Cropp <simon.cropp@gmail.com>
2025-10-12 21:31:52 +1100

    Update GlobalUsings.cs

--- src/ExcelsiorAspose/GlobalUsings.cs
+++ src/ExcelsiorAspose/GlobalUsings.cs
@@ -1,6 +1,5 @@
 ﻿global using System.Drawing;
 global using Aspose.Cells;
-global using System.Net;
 global using Excelsior;
 global using Sheet = Aspose.Cells.Worksheet;
 global using Book = Aspose.Cells.Workbook;

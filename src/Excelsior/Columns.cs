433f1417b091ccce5e4e56a1aa5a7f28fca14f1a
Simon Cropp <simon.cropp@gmail.com>
2025-10-13 11:21:28 +1100

    Update Columns.cs

--- src/Excelsior/Columns.cs
+++ src/Excelsior/Columns.cs
@@ -6,28 +6,33 @@
     {
         foreach (var property in Properties<TModel>.Items)
         {
-            var type = property.Type;
-            var render = ValueRenderer.GetRender(type);
-
-            columns.Add(
-                property.Name,
-                new()
-                {
-                    Name = property.Name,
-                    Order = property.Order,
-                    Heading = property.DisplayName,
-                    Width = property.Width,
-                    HeadingStyle = null,
-                    CellStyle = null,
-                    Format = property.Format,
-                    NullDisplay = property.NullDisplay ?? ValueRenderer.GetNullDisplay(type),
-                    Render = render == null ? null : (_, value) => render(value),
-                    IsHtml = property.IsHtml,
-                    IsNumber = property.IsNumber,
-                    IsEnumerableString = type.IsAssignableTo(typeof(IEnumerable<string>)),
-                    GetValue = property.Get
-                });
+            AddColumn(property);
         }
+    }
+
+    void AddColumn(Property<TModel> property)
+    {
+        var type = property.Type;
+        var render = ValueRenderer.GetRender(type);
+
+        columns.Add(
+            property.Name,
+            new()
+            {
+                Name = property.Name,
+                Order = property.Order,
+                Heading = property.DisplayName,
+                Width = property.Width,
+                HeadingStyle = null,
+                CellStyle = null,
+                Format = property.Format,
+                NullDisplay = property.NullDisplay ?? ValueRenderer.GetNullDisplay(type),
+                Render = render == null ? null : (_, value) => render(value),
+                IsHtml = property.IsHtml,
+                IsNumber = property.IsNumber,
+                IsEnumerableString = type.IsAssignableTo(typeof(IEnumerable<string>)),
+                GetValue = property.Get
+            });
     }
 
     public void Add<TProperty>(

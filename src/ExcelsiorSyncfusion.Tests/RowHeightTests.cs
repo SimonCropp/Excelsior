3d832007297817dd3ee35739436a41c4c4078ce5
Simon Cropp <simon.cropp@gmail.com>
2026-01-06 12:43:06 +1100

    .

---
+++ src/ExcelsiorSyncfusion.Tests/RowHeightTests.cs
@@ -0,0 +1,35 @@
+[TestFixture]
+public class RowHeightTests
+{
+    public record Target(string LongText);
+
+    [Test]
+    public async Task RowHeightShouldNotExceedMaximum()
+    {
+        var bookBuilder = new BookBuilder();
+
+        // Create a very long text that would cause row height to exceed 409 when wrapped
+        var longText = string.Join("\n", Enumerable.Range(1, 100).Select(i => $"Line {i}: This is a very long line of text that will wrap"));
+
+        List<Target> data =
+        [
+            new(longText),
+        ];
+
+        var sheetBuilder = bookBuilder.AddSheet(data);
+        sheetBuilder.Column(_ => _.LongText, _ => _.Width = 30);
+
+        using var book = await bookBuilder.Build();
+
+        // Verify that no row height exceeds the Excel maximum of 409
+        var sheet = book.Worksheets[0];
+        var lastRow = sheet.UsedRange.LastRow;
+
+        for (var i = 1; i <= lastRow; i++)
+        {
+            var height = sheet.Range[i, 1].RowHeight;
+            Assert.That(height, Is.LessThanOrEqualTo(409),
+                $"Row {i} height {height} exceeds maximum allowed height of 409");
+        }
+    }
+}

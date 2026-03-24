66bc6be4a8b7d4cfa2bf187a01e5498ae96595c4
Simon Cropp <simon.cropp@gmail.com>
2026-03-24 21:48:36 +1100

    SheetModel type must be accessible (#144)

--- src/Excelsior.SourceGenerator/ModelInfo.cs
+++ src/Excelsior.SourceGenerator/ModelInfo.cs
@@ -2,3 +2,11 @@ record struct ModelInfo(
     string TypeFullName,
     string TypeName,
     EquatableArray<PropertyInfo> Properties);
+
+record struct ModelResult(
+    ModelInfo? Model,
+    DiagnosticInfo? Diagnostic);
+
+record struct DiagnosticInfo(
+    string TypeName,
+    Location? Location);

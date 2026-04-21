51f0acf545ba96dc092e9dcb8a24512c0bea3ba6
Simon Cropp <simon.cropp@gmail.com>
2026-04-22 09:14:02 +1000

    more tests

--- src/Excelsior.Tests/NaughtyStringsTests.cs
+++ src/Excelsior.Tests/NaughtyStringsTests.cs
@@ -2,17 +2,20 @@
 [TestFixture]
 public class NaughtyStringsTests
 {
-    record Row(string Value);
+    record Row(string Value, string Html);
 
     [Test]
     public async Task Test()
     {
         var rows = TheNaughtyStrings.All
-            .Select(_ => new Row(_))
+            .Select(_ => new Row(_, _))
             .ToList();
 
         var builder = new BookBuilder();
-        builder.AddSheet(rows);
+        var sheetBuilder = builder.AddSheet(rows);
+        sheetBuilder.Column(
+            _ => _.Html,
+            _ => _.IsHtml = true);
 
         var book = await builder.Build();
 
